c9030ad415e304b20a5549ce5aebbfaeca17e76e
thisisgenaro <genaro.hernandez@outlook.com>
2022-04-18 16:34:24 -0400

    Add Experience Scripts

--- scripts.js
+++ scripts.js
@@ -2,13 +2,47 @@ const experience_data = "./experience.json";
 
 function importExperienceData()
 {
-    let response = await fetch(experience_data);
-    let data = await response.json();
 
-    console.log(data);
+    fetch("./experience.json")
+    .then(response => {
+        return response.json();
+    })
+    .then(data => logExperienceData(data));
+
 }
 
-function helloworld()
+function logExperienceData(data)
 {
-    console.log("Hello World");
+    data.forEach(writeExperience)
+}
+
+function writeExperience(experience)
+{
+
+    var timespan = experience.timespan;
+    var role = experience.role;
+    var company = experience.company;
+    var description = experience.description;
+    
+    var AddLi = document.createElement('li');
+    var timespanNew = document.createElement('div');
+    timespanNew.innerText = timespan;
+    AddLi.appendChild(timespanNew);
+
+    var roleNew = document.createElement('strong');
+    var companyNew = document.createElement('em');
+    
+    roleNew.innerText = role;
+    companyNew.innerText = company;
+
+    var content = document.createElement('div');
+    
+    content.appendChild(roleNew);
+    content.appendChild(companyNew);
+    content.innerHTML += description;
+    
+    AddLi.appendChild(content);
+
+	document.getElementById("work-experience").appendChild(AddLi);
+
 }
